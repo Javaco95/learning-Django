41a9207a2987a58d501e58a51c28e5bd9d509240
Javaco95 <127981406+Javaco95@users.noreply.github.com>
2023-07-03 15:24:02 -0600

    030723

--- myapp/views.py
+++ myapp/views.py
@@ -1,25 +1,37 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Index page")
+    title= 'Django Course!'
+    return render(request, 'index.html', {
+        'title': title
+    })
 
 def hello(request, username):
     return HttpResponse("<h1>Hello %s</h1>" % username)
 
 def about(request):
-    return HttpResponse('About')
+    username = 'Javaco95'
+    return render(request, 'about.html', {
+        'username' : username
+    })
 
 
 def projects(request):
-    projects = list(Project.objects.values())
-    return JsonResponse(projects, safe=False)
+    #projects = list(Project.objects.values())
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
 
-def tasks(request, id):
-    task = get_object_or_404(Task, id=id)
-    return HttpResponse('task: %s ' % task.title)
+def tasks(request):
+    #task = get_object_or_404(Task, id=id)
+    tasks = Task.objects.all()
+    return render(request, 'tasks.html', {
+        'tasks': tasks
+    })
     
