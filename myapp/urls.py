1e11d537a8ddf577ee8aee6ae1be18be2f38cf25
Javaco95 <127981406+Javaco95@users.noreply.github.com>
2023-07-10 18:11:10 -0600

    django 100723

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,10 +4,12 @@ from myapp import views
 
 
 urlpatterns = [
-    path('', views.index),
-    path('about/', views.about),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('hello/<str:username>', views.hello),
-    path('projects/', views.projects),
-    path('tasks/', views.tasks),
-    
+    path('projects/', views.projects, name='projects'),
+    path('projects/<int:id>', views.project_detail, name='project_detail'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('create_task/', views.create_task, name='create_task'),
+    path('create_project/', views.create_project, name='create_project'),
 ]
